dfbd0b4e45d1d2e97502fc6a5621da2458e9b7b5
RyanSullivan-prog <82338284+RyanSullivan-prog@users.noreply.github.com>
2025-03-05 11:57:37 -0500

    Create open button

--- JUCE_SRC/Source/MainComponent.h
+++ JUCE_SRC/Source/MainComponent.h
@@ -2,12 +2,14 @@
 
 #include <JuceHeader.h>
 
+using namespace juce;
+
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::Component
+class MainComponent  : public juce::AudioAppComponent
 {
 public:
     //==============================================================================
@@ -15,13 +17,18 @@ public:
     ~MainComponent() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+    //==============================================================================
+    void paint (juce::Graphics& g) override;
     void resized() override;
 
 private:
     //==============================================================================
     // Your private member variables go here...
-
+    TextButton openButton;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
