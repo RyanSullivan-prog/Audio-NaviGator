c970772836ab5b7f98e6c9f10ef9ed3180001d5d
RyanSullivan-prog <82338284+RyanSullivan-prog@users.noreply.github.com>
2025-03-31 13:33:55 -0400

    Add files with startup code

---
+++ JUCE_SRC/Equalizer_Src/PluginEditor.h
@@ -0,0 +1,35 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+// equalizer code created using https://www.youtube.com/watch?v=i_Iq4_Kd7Rc&list=WL&index=4
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class NewProjectAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    NewProjectAudioProcessorEditor (NewProjectAudioProcessor&);
+    ~NewProjectAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    NewProjectAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NewProjectAudioProcessorEditor)
+};
